58d8cead0b5821f3aec051c5fd247bd73b8c09ce
Caravaggi <126033275+Caravaggis@users.noreply.github.com>
2026-01-29 14:01:39 -0800

    Add event listeners for yes and no buttons

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+  document.body.innerHTML = `
+    <h1 style="text-align:center; margin-top:40vh;">
+      🌹 You just made me very happy 🌹
+    </h1>
+  `;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
 
